6fc2e9bb43c82bbd377aca9383fe91371292bd70
yuyuayush <ayushnegi1912@gmail.com>
2024-02-05 11:45:30 +0530

    email

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -57,7 +57,7 @@ export async function POST(req: Request) {
       const { id,email_addresses,image_url ,first_name,last_name,username } = evt.data;
       const  user ={ 
         clerkId: id,
-        email: email_addresses[0]!,
+        email: email_addresses[0].email_address,
         username: username!,
         firstName: first_name!,
         lastName : last_name!,
